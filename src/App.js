d1e5ded149263615f2193e0d600ff073337937c7
Christian Aquino <cjfaquino@gmail.com>
2020-10-13 23:31:28 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Slidertest from './Components/slidertest';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Slidertest />
+    </div>
+  );
+}
+
+export default App;
